a75d2dee6b7a4fc121d05357bbd5b99bf800156c
Jimmy Chen <jchenex4@gmail.com>
2016-10-28 16:00:00 -0500

    Add files via upload

---
+++ Homework_5/Homework_5/Utility.cpp
@@ -0,0 +1,14 @@
+#include "StdAfx.h"
+#include "Utility.h"
+using namespace std;
+using namespace System;
+
+Utility::Utility(void)
+{
+}
+/*
+string Utility::StringToStdString(String^ str)
+{
+	return msclr::interop::marshal_as<std::string>(str);
+}
+*/
---
+++ Homework_5/Homework_5/Utility.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+using namespace std;
+using namespace System;
+
+class Utility
+{
+public:
+	Utility(void);
+//	static string StringToStdString(String^ str);
+};
+
